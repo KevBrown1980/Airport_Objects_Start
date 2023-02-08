cb4116ebd9e71596b15d08b4c8b9abaa970ac473
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-08 22:34:47 +0000

    fixing errors

--- Person.js
+++ Person.js
@@ -5,6 +5,17 @@ class Person {
             // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
     // Include an addBag method
+
+    constructor(name, bags) {
+        this.name = name;
+        this.bags = [];
+    }
+
+    addBags (bags) {
+        
+            this.bags.push(bags)
+        }    
 }
 
 //Export the Module
+module.exports = Person;
